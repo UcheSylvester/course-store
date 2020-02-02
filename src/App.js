1d2b1ebba19026b54ce857cb1c2840436822c194
UcheSylvester <okorocode@gmail.com>
2020-02-02 15:37:32 +0100

    adding header for routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import AboutPage from './pages/about-page/about-page.component';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 const App = () => {
-  const route = window.location.pathname;
 
-  return route === '/about' ? <AboutPage /> : <HomePage />
+  const getPage = () => {
+    const route = window.location.pathname;
+    return route === '/about' ? <AboutPage /> : <HomePage />
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  )
+
+
 }
 
 export default App;
