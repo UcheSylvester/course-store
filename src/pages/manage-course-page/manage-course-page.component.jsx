85bc5f68ab47e22f39b9530d63165eb8d642b292
UcheSylvester <okorocode@gmail.com>
2020-02-02 18:03:27 +0100

    introducing router Prompt component for displaying messages before user leaves a page

--- src/pages/manage-course-page/manage-course-page.component.jsx
+++ src/pages/manage-course-page/manage-course-page.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = props => {
   console.log(props);
@@ -6,6 +7,7 @@ const ManageCoursePage = props => {
   return (
     <>
       <h2>Manage Courses</h2>
+      <Prompt when={true} message="Are you sure you want to leave" />
       <p>{props.match.params.slug} </p>
     </>
   );
