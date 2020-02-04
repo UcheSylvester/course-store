09ed0fec5a6ab8d139d6ff498c652e7c836eedfa
UcheSylvester <okorocode@gmail.com>
2020-02-05 00:40:30 +0100

    created saveCourse action and dispatched it using the flux dispatcher

---
+++ src/flux/app.dispatcher.js
@@ -0,0 +1,5 @@
+import { Dispatcher } from "flux";
+
+const disptcher = new Dispatcher();
+
+export default disptcher;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import App from "./App";
 
 ReactDom.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>, document.getElementById('root'))
+  </BrowserRouter>,
+  document.getElementById("root")
+);
